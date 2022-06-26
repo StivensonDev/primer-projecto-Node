3e2589b44fc8099bf11c062e022eba88b08cdf1c
STIVENSON.DEV <96761629+StivensonSS@users.noreply.github.com>
2022-06-25 23:06:42 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+{
+  "name": "hola-mundo",
+  "version": "1.0.0",
+  "description": "Este es el primer projecto de JS con Node",
+  "main": "index.js",
+  "scripts": {
+    "start": "Hola este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo",
+    "saludar":"node hola_mundo.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [
+    "holamundo",
+    "firtsproject"
+  ],
+  "author": "Stivenson Sosa",
+  "license": "ISC"
+}
